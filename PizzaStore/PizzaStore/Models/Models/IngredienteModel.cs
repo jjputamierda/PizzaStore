44f62b95585e3c32fff4d946ac708cf32eddd951
jjputamierda <jjherrerarodriguez98@gmail.com>
2020-07-07 19:56:59 -0600

    Se crea el modelo del ingrediente

---
+++ PizzaStore/PizzaStore/Models/Models/IngredienteModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzaStore.Models.Models
+{
+    public class IngredienteModel
+    {
+        [BindProperty]
+        public string  nombre{ get; set; }
+
+        [BindProperty]
+
+        public double precio { get; set; }
+    }
+}
