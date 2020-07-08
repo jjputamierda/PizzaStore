d993d2987af98d8df27615cadc0ec3a6f0e88586
jjputamierda <jjherrerarodriguez98@gmail.com>
2020-07-07 21:45:17 -0600

    Se modifica Toast Option para que solo guarde la opcion de tostado, el precio ahi no tiene sentido

--- PizzaStore/PizzaStore/Models/ToastOptionModel.cs
+++ PizzaStore/PizzaStore/Models/ToastOptionModel.cs
@@ -5,7 +5,8 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace PizzaStore.Models.Models
+namespace PizzaStore.Models
+
 {
     public class ToastOptionModel
     {
