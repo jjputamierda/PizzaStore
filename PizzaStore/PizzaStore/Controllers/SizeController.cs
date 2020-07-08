dbcd5ebd73659310f9ec2bcc2fe2ef21e6f6e64b
jjputamierda <jjherrerarodriguez98@gmail.com>
2020-07-08 09:43:30 -0600

    Se agrega Size Controller para controlar los tamaños de las pizzas

---
+++ PizzaStore/PizzaStore/Controllers/SizeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PizzaStore.Models.Handlers;
+using PizzaStore.Models;
+
+
+namespace PizzaStore.Controllers
+{
+    public class SizeController
+    {
+        public SizeHandler sizeHandler;
+        public SizeController()
+        {
+            sizeHandler = new SizeHandler();
+        }
+
+        public int VerifySize(List<SizeModel> size)
+        {
+            int answer = sizeHandler.VerifySize(size);
+            return answer;
+        }
+
+        public double GetPriceSize(List<SizeModel> sizes)
+        {
+
+
+
+            return sizeHandler.GetPriceSize(sizes);
+        }
+        public List<SizeModel> InitSizes()
+        {
+
+
+            return sizeHandler.InitSizes();
+
+
+        }
+        public List<SizeModel> NewSize(List<SizeModel> sizes, List<string> names)
+        {
+            
+            return sizeHandler.NewSize(sizes,names);
+        }
+    }
+
+
+}
+
+
