0ebe713dd38a1c755e721ca5afe45a70acc5fcec
jjputamierda <jjherrerarodriguez98@gmail.com>
2020-07-07 23:47:16 -0600

    Se añade avisos inmediatos al programa para poder mostrar mensajes de error correctamente

---
+++ PizzaStore/PizzaStore/Utilidades/AvisosInmediatos.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace PizzaStore.Utilidades
+{
+    public static class AvisosInmediatos
+    {
+        public enum TipoAviso
+        {
+            Exito = 0,
+            Error = 1
+        }
+
+        public static void Set(PageModel page, string nombre, string mensaje, TipoAviso tipo)
+        {
+
+
+            if (tipo == TipoAviso.Exito)
+            {
+                page.TempData[nombre] = "<div class=\"alert alert-success\"role=\"alert\">" +
+                                        mensaje +
+                                        "</div>";
+            }
+            else
+            {
+                page.TempData[nombre] = "<div class=\"alert alert-danger\"role=\"alert\">" +
+                                        "<span class=\"glyphicon glyphicon-exclamation-sign\" aria-hidden=\"true\"></span>" +
+                                        "<span class=\"sr-only\">Error:</span>" + " " +
+                                        mensaje +
+                                        "</div>";
+            }
+
+        }
+
+        public static bool Existe(PageModel page, string nombre)
+        {
+            return page.TempData[nombre] != null;
+        }
+
+        public static string Get(PageModel page, string nombre)
+        {
+            return (string)page.TempData[nombre];
+        }
+    }
+}
