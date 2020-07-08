21773c1fe128fa117553dfe9a6263ee4c03650a6
jjputamierda <jjherrerarodriguez98@gmail.com>
2020-07-07 22:12:51 -0600

    Se crea controlador de ingredientes

---
+++ PizzaStore/PizzaStore/Controllers/IngredientController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PizzaStore.Models.Handlers;
+using PizzaStore.Models;
+
+
+namespace PizzaStore.Controllers
+{
+    public class IngredientController
+    {
+        public IngredientHandler ingredientHandler;
+        public IngredientController() {
+            ingredientHandler = new IngredientHandler();
+        }
+
+        public int VerifyIngredients(List<IngredientModel> ingredients) {
+            int answer = ingredientHandler.VerifyIngredients(ingredients);
+            return answer;
+        }
+
+        public double GetPriceIngredients(List<IngredientModel> ingredients)
+        {
+            
+            
+
+            return ingredientHandler.GetPriceIngredients(ingredients);
+        }
+        public List<IngredientModel> InitIngredients()
+        {
+           
+     
+            return ingredientHandler.InitIngredients();
+
+
+        }
+
+    }
+
+
+}
+
