4c137175bdca775e78fd5e0552568ccde39ff894
jjputamierda <jjherrerarodriguez98@gmail.com>
2020-07-07 23:08:28 -0600

    Se crea el modelo de la pizza

---
+++ PizzaStore/PizzaStore/Models/PizzaModel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzaStore.Models
+{
+    public class PizzaModel
+    {
+        [BindProperty]
+        public List <IngredientModel> ingredients{ get; set; }
+
+        [BindProperty]
+
+        public List<MassModel> mass { get; set; }
+
+        [BindProperty]
+
+        public List<ToastOptionModel> tostOption { get; set; }
+    }
+}
