2415c2a058e45f0c1a7968421fb3a50f7df9890e
jjputamierda <jjherrerarodriguez98@gmail.com>
2020-07-07 22:13:03 -0600

    Se crea controlador de masa

---
+++ PizzaStore/PizzaStore/Controllers/MassController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PizzaStore.Models.Handlers;
+using PizzaStore.Models;
+
+
+namespace PizzaStore.Controllers
+{
+    public class MassController
+    {
+        public MassHandler massHandler;
+        public MassController()
+        {
+            massHandler = new MassHandler();
+        }
+
+        public int VerifyMass(List<MassModel> mass)
+        {
+            int answer = massHandler.VerifyMass(mass);
+            return answer;
+        }
+
+        public double GetPriceMass(List<MassModel> masses)
+        {
+
+
+
+            return massHandler.GetPriceMass(masses);
+        }
+        public List<MassModel> InitMass()
+        {
+
+
+            return massHandler.InitMasses();
+
+
+        }
+
+    }
+
+
+}
+
+
