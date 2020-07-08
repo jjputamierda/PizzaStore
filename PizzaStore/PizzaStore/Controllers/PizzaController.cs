5775b8e98df7424b8fd8b3e0f64a779074b6cb25
jjputamierda <jjherrerarodriguez98@gmail.com>
2020-07-07 23:08:53 -0600

    Se crea el controlador de la pizza

---
+++ PizzaStore/PizzaStore/Controllers/PizzaController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PizzaStore.Models.Handlers;
+using PizzaStore.Models;
+
+
+namespace PizzaStore.Controllers
+{
+    public class PizzaController
+    {
+        public PizzaHandler pizzaHandler;
+        public PizzaController() {
+            pizzaHandler = new PizzaHandler();
+        }
+
+        
+        public PizzaModel InitPizza(List<IngredientModel> ingredientModels, List<MassModel> mass, List<ToastOptionModel> toastOptions)
+        {
+
+
+            return pizzaHandler.InitPizza(ingredientModels,mass,toastOptions);
+
+
+        }
+
+        public List<PizzaModel> DeletePizza(List<PizzaModel> pizzas, int pos)
+        {
+
+
+            return pizzaHandler.DeletePizza(pizzas,pos);
+
+
+        }
+
+        public  List <PizzaModel> AddPizza(List<PizzaModel> pizzas, PizzaModel pizza)
+        {
+
+            return pizzaHandler.AddPizza(pizzas,pizza);
+
+
+        }
+
+        public double GetPricePizza(List<PizzaModel> pizzas, IngredientController ingredientController, MassController massController)
+        {
+          
+            return pizzaHandler.GetPricePizza(pizzas,ingredientController,massController);
+        } 
+    }
+
+
+}
+
