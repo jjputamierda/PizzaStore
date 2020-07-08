e812aae522f1b1e39394ab141957326142735069
jjputamierda <jjherrerarodriguez98@gmail.com>
2020-07-07 22:13:19 -0600

    Se crea controlador de opciones de tostado

---
+++ PizzaStore/PizzaStore/Controllers/ToastOptionController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PizzaStore.Models.Handlers;
+using PizzaStore.Models;
+
+
+namespace PizzaStore.Controllers
+{
+    public class ToastOptionController
+    {
+        public ToastOptionHandler toastOptionHandler;
+        public ToastOptionController() {
+            toastOptionHandler = new ToastOptionHandler();
+        }
+
+        public int VerifyToastOption(List<ToastOptionModel> toastOption) {
+            int answer = toastOptionHandler.VerifyToastOption(toastOption);
+            return answer;
+        }
+
+        public string GetToastOptions(List<ToastOptionModel> toastOption)
+        {
+            
+            
+
+            return toastOptionHandler.GetToastOptions(toastOption);
+        }
+        public List<ToastOptionModel> InitToastOptions()
+        {
+           
+     
+            return toastOptionHandler.InitToastOption();
+
+
+        }
+
+    }
+
+
+}
+
