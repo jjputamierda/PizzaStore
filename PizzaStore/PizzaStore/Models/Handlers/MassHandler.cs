1680f7735697842f396eafc470c73d634de49f45
jjputamierda <jjherrerarodriguez98@gmail.com>
2020-07-07 21:47:30 -0600

    Se crea El controlador de la masa

---
+++ PizzaStore/PizzaStore/Models/Handlers/MassHandler.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PizzaStore.Models;
+
+
+namespace PizzaStore.Models.Handlers
+{
+    public class MassHandler
+    {
+        public MassHandler()
+        {
+        }
+
+        public int VerifyMass(List<MassModel> masses)
+        {
+            int answer = 0;
+            if (masses.Count() > 0)
+            {
+                answer = 1;
+            }
+            return answer;
+        }
+
+        public double getPriceMass(List<MassModel> masses)
+        {
+            double totalPrice = 0;
+
+            foreach (var mass in masses)
+            {
+                totalPrice = mass.price + totalPrice;
+            }
+
+
+            return totalPrice;
+        }
+        public List<MassModel> InitMasses()
+        {
+            List<MassModel> masses = new List<MassModel> {
+            new MassModel { name ="thin crust", price = 700  },
+            new MassModel { name ="classic", price = 700  }
+        };
+
+            return masses;
+
+
+        }
+
+    }
+
+
+}
