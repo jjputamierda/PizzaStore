b4a8fc34901f549b39fca3d81109af7a28c1f846
jjputamierda <jjherrerarodriguez98@gmail.com>
2020-07-07 21:47:53 -0600

    Se crea el controlador de las opciones de tostado

---
+++ PizzaStore/PizzaStore/Models/Handlers/ToastOptionHandler.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PizzaStore.Models;
+
+
+namespace PizzaStore.Models.Handlers
+{
+    public class ToastOptionHandler
+    {
+        public ToastOptionHandler() {
+        }
+
+        public int VerifyIngredients(List<ToastOptionModel> toastOptions) {
+            int answer = 0;
+            if (toastOptions.Count() > 0) {
+                answer = 1;
+            }
+            return answer;
+        }
+
+        public string getToastOptions(List<ToastOptionModel> toastOptions)
+        {
+           
+            
+
+            return toastOptions[0].name;
+        }
+        public List<ToastOptionModel> InitToastOption()
+        {
+            List<ToastOptionModel> toastOption = new List<ToastOptionModel> {
+            new ToastOptionModel { name ="crunchy" },
+            new ToastOptionModel { name ="soft" },
+            new ToastOptionModel { name ="middle"}
+            
+        };
+
+            return toastOption;
+
+
+        }
+
+    }
+
+
+}
+
