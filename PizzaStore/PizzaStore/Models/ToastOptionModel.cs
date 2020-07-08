94fb2a75df52a46e330610fc1e2bcdf6d28d13ee
jjputamierda <jjherrerarodriguez98@gmail.com>
2020-07-07 20:18:34 -0600

    Se crea el modelo para las opciones de tostado de la pizza

---
+++ PizzaStore/PizzaStore/Models/ToastOptionModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzaStore.Models.Models
+{
+    public class ToastOptionModel
+    {
+        [BindProperty]
+        public string  name{ get; set; }
+
+
+    }
+}
