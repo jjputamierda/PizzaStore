de2429339f4709d28abfe54f0dc78805707898b4
jjputamierda <jjherrerarodriguez98@gmail.com>
2020-07-07 20:24:07 -0600

    Se adjunta el modelo de la ubicacion general

---
+++ PizzaStore/PizzaStore/Models/GeneralUbicationModel.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzaStore.Models
+{
+    public class GeneralUbicationModel
+    {
+        [BindProperty]
+        public string provincia{ get; set; }
+
+        [BindProperty]
+
+        public double canton{ get; set; }
+
+
+    }
+}
