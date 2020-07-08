740a250dfd5e59023edb8fd0f33c182289c88da7
jjputamierda <jjherrerarodriguez98@gmail.com>
2020-07-07 21:45:57 -0600

    Se modifica el modelo de masa para que los tipos de sus variables sean acordes a lo que se necesita

--- PizzaStore/PizzaStore/Models/MassModel.cs
+++ PizzaStore/PizzaStore/Models/MassModel.cs
@@ -5,12 +5,16 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace PizzaStore.Models.Models
+namespace PizzaStore.Models
 {
     public class MassModel
     {
         [BindProperty]
         public string  name{ get; set; }
+
+        [BindProperty]
+
+        public double price { get; set; }
 
 
     }
